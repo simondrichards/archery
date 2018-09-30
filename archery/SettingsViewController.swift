cac87ff56cdb7687075d0f67bc9c7d54e55e8bac
Simon Richards <s.richards@physics.org>
2018-09-30 08:28:37 +0100

    Add settings view controller

---
+++ archery/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  archery
+//
+//  Created by Simon Richards on 30/09/2018.
+//  Copyright © 2018 Simon Richards. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("SettingsViewController")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
